289e4da5c50592797ba65548f25a8ddc6b84eb2e
sujalchavdaa <sujalchavda500@gmail.com>
2025-09-27 21:50:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Bot configuration
-API_ID = "23801870"
-API_HASH = "9645cfafdfc9be9a7a46fb4874992cf6"
-BOT_TOKEN = "8415006582:AAED2O-IZxPOIwetOe0EqShCOmWj7sJ0Y-s"
+API_ID = "25933223"
+API_HASH = "6ef5a426d85b7f01562a41e6416791d3"
+BOT_TOKEN = "8028954491:AAFrtHL0u017Et-jKXBMvTaMNpG9BgInLI4"
 
 # Channel info
-CHANNEL_USERNAME = "SDV_BOTz"
+CHANNEL_USERNAME = "SDV_BOTgz"
 
 # Authorized users
-AUTH_USERS = [6877021488,6005627579]  # Your user ID and any other admins
+AUTH_USERS = [8458169280]  # Your user ID and any other admins
