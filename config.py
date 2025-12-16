a74df718e0620eeed55c290400061253b67c21ba
kabaci1299 <basamsenha@gmail.com>
2025-12-16 09:15:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Bot configuration
-API_ID = "25933223"
-API_HASH = "6ef5a426d85b7f01562a41e6416791d3"
-BOT_TOKEN = "8028954491:AAFrtHL0u017Et-jKXBMvTaMNpG9BgInLI4"
+API_ID = "26375665"
+API_HASH = "568839157ce65f4d3a91647f022b6737"
+BOT_TOKEN = "8560935630:AAG--ms9I5ktXpkJt311j1pw1W1HlRFH2_M"
 
 # Channel info
 CHANNEL_USERNAME = "SDV_BOTgz"
 
 # Authorized users
-AUTH_USERS = [8458169280]  # Your user ID and any other admins
+AUTH_USERS = [6834250190]  # Your user ID and any other admins
